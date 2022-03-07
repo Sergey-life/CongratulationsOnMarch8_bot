04c5a52b422e3ae3783ae1ded1d2d9ec671ceeb5
Sergey-life <sergei.kharchenko888@gmail.com>
2022-03-07 22:10:06 +0200

    first commit

---
+++ const.js
@@ -0,0 +1,19 @@
+const commands = `
+/start - Перезавантажити бота
+/help - Допомога
+/interesting - Цікава інформація про це свято
+`
+const infoText = `
+Міжнародний жіночий день <a href="https://uk.wikipedia.org/wiki/%D0%9C%D1%96%D0%B6%D0%BD%D0%B0%D1%80%D0%BE%D0%B4%D0%BD%D0%B8%D0%B9_%D0%B6%D1%96%D0%BD%D0%BE%D1%87%D0%B8%D0%B9_%D0%B4%D0%B5%D0%BD%D1%8C">Дивитися</a>
+`
+const infoMusic = `
+Відео привітання - <a href="https://www.youtube.com/embed/7s_hXAhINhQ">Дивитися</a>
+`
+const infoVideo = `
+Слухати музику - <a href="https://www.youtube.com/embed/j26ct9qjFQE">перейти</a>
+`
+
+module.exports.commands = commands
+module.exports.infoText = infoText
+module.exports.infoMusic = infoMusic
+module.exports.infoVideo = infoVideo
---
+++ index.js
@@ -0,0 +1,47 @@
+const { Telegraf, Markup } = require('telegraf')
+require('dotenv').config()
+const botCommands = require('./const')
+
+const bot = new Telegraf(process.env.BOT_TOKEN)
+bot.start((ctx) => ctx.reply(`Привет! ${ctx.message.from.first_name ? ctx.message.from.first_name : 'Незнакомец'}!`))
+bot.help((ctx) => ctx.reply(botCommands.commands))
+bot.command('interesting', async (ctx) => {
+    try {
+        await ctx.replyWithHTML('<strong>Про це свято</strong>', Markup.inlineKeyboard(
+            [
+                [Markup.button.callback('Цікаві факти', 'btn_1')],
+                [Markup.button.callback('Музика', 'btn_2'), Markup.button.callback('Відео', 'btn_3')]
+            ]
+        ))
+    } catch (e) {
+        console.error(e)
+    }
+})
+
+function addActionBot(idButton, src, data) {
+    bot.action(idButton, async (ctx) => {
+        try {
+            await ctx.answerCbQuery()
+            if (src !== false) {
+                await ctx.replyWithPhoto({
+                    source: src
+                })
+            }
+            await ctx.replyWithHTML(data, {
+                disable_web_page_preview: true
+            })
+        } catch (e) {
+            console.error(e)
+        }
+    })
+}
+
+addActionBot('btn_1', './img/1.jpg', botCommands.infoText)
+addActionBot('btn_2', './img/2.jpg', botCommands.infoMusic)
+addActionBot('btn_3', './img/3.jpg', botCommands.infoVideo)
+
+bot.launch()
+
+// Enable graceful stop
+process.once('SIGINT', () => bot.stop('SIGINT'))
+process.once('SIGTERM', () => bot.stop('SIGTERM'))
